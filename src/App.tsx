ee76fd2d40a83837263fb4ed69b65030181258dd
Daphnee Jeune <daphneejeune.dev@gmail.com>
2024-11-18 17:18:23 -0600

    Make login page the default

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Navigate replace to="/home" />}></Route>
+        <Route path="/" element={<Navigate replace to="/login" />}></Route>
         <Route path="/home" element={<Home />}></Route>
         <Route path="/login" element={<Login />}></Route>
       </Routes>
